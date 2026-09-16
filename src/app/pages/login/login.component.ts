2a1f34b169d88cc772278bb5e6d6289502820593
GABRIEL MATIAS NUNES <gabriel.mnunes3@senacsp.edu.br>
2026-09-15 22:26:42 -0300

    fazer login e senha

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
+
+  botaoDesabilitado:boolean = true;
+  login: string= "";
+  senha: string= "";
+
+  validarFormulario(){
+    if(this.login.trim()!=="" && this.senha.trim() != ""){
+       this.botaoDesabilitado = false;
+
+    }else{
+      this.botaoDesabilitado = true;
+    }
+
+  }
+
+
+  fazerLogin(){
+
+  }
+  
+  
+  
+  
+  //quando acionar o botao, chama esse metodo
+  onBotaoClicado(){
+    alert("botão clicado ");
+    
+  }
+  teclaDigitada(event: KeyboardEvent): void {
+ alert("o usuario digitol    " + event.key)
+ }
+
 
 }
